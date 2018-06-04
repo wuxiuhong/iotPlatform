8dfe8186a50d24f27e0aea2569dad7e724a11a40
wendy_wu <852648237@qq.com>
2018-06-04 18:57:23 +0800

    修改typescript

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -6,6 +6,7 @@ var vueLoaderConfig = require('./vue-loader.conf');
 function resolve(dir) {
     return path.join(__dirname, '..', dir);
 }
+process.noDeprecation = true;
 
 module.exports = {
     entry: {
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import Vue from 'vue';
-import App from '../../App.vue';
+import App from './App.vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import VueRouter from 'vue-router';
 import store from './vuex/store';
 import Vuex from 'vuex';
 import routes from './routes';
-import Mock from './mock/index';
-import SoulUi from './util/install_component';
+import Mock from './mock';
+import SoulUi from './util/install_component.js';
 
 Mock.bootstrap();
 import 'font-awesome/css/font-awesome.min.css';
