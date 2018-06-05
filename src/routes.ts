9d695af04d8459251a6bae7f1b7695417bdf6ae3
wendy_wu <852648237@qq.com>
2018-06-05 14:18:02 +0800

    读取json数据1

--- src/routes.ts
+++ src/routes.ts
@@ -40,45 +40,4 @@ const routes = [
         redirect: {path: '/404'}
     }
 ];
-// {
-//     path: '/',
-//         component: Home,
-//     name: '导航一',
-//     iconCls: 'fa fa-envelope-o', // 图标样式class
-//     children: [
-//     {path: '/main', component: Main, name: '主页', hidden: true},
-//     {path: '/table', component: Table, name: 'Table'},
-//     {path: '/form', component: Form, name: 'Form'},
-//     {path: '/user', component: user, name: '列表'},
-// ]
-// },
-// {
-//     path: '/',
-//         component: Home,
-//     name: '导航二',
-//     iconCls: 'fa fa-id-card-o',
-//     children: [
-//     {path: '/page4', component: Page4, name: '页面4'},
-//     {path: '/page5', component: Page5, name: '页面5'}
-// ]
-// },
-// {
-//     path: '/',
-//         component: Home,
-//     name: '',
-//     iconCls: 'fa fa-address-card',
-//     leaf: true, // 只有一个节点
-//     children: [
-//     {path: '/page6', component: Page6, name: '导航三'}
-// ]
-// },
-// {
-//     path: '/',
-//         component: Home,
-//     name: 'Charts',
-//     iconCls: 'fa fa-bar-chart',
-//     children: [
-//     {path: '/echarts', component: echarts, name: 'echarts'}
-// ]
-// }
 export default routes;
