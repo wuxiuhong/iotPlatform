698e01fc72b5b023cbeac40a41f155ec51240ebe
wendy.wu <852648237@qq.com>
2018-06-21 02:14:47 +0800

    测试echart的json格式

--- src/api/template.ts
+++ src/api/template.ts
@@ -4,5 +4,5 @@ const base = '/api';
 
 // 获取报表数据
 export const getTemplate = params => {
-    return axios.get(`${base}/templates/3f825d20-fe01-11e7-83b5-9b9f49d154f1`, {params: params});
+    return axios.get(`${base}/templates/2`, {params: params});
 };
